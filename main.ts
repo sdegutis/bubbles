e63b1f6612fb4f6db7493519978113368151f423
Steven Degutis <sbdegutis@gmail.com>
2025-05-26 17:59:42 -0500

    Prep.

--- main.ts
+++ main.ts
@@ -1,45 +1,19 @@
-/// <reference types="immaculata/jsx-strings.js" />
-/// <reference types="immaculata/jsx-strings-html.js" />
+import { DevServer, FileTree, Pipeline } from "immaculata"
+import { stripTypeScriptTypes } from "module"
 
-import { DevServer, FileTree } from "immaculata"
-import { compileJsx, mapImport, tryAltExts, useTree } from "immaculata/hooks.js"
-import { registerHooks } from "module"
-import ts from 'typescript'
-import { fileURLToPath } from "url"
-
-const src = new FileTree('src', import.meta.dirname)
-const data = new FileTree('data', import.meta.dirname)
+const site = new FileTree('site', import.meta.dirname)
 const server = new DevServer(8181)
 
-registerHooks(mapImport('react/jsx-runtime', 'immaculata/jsx-strings.js'))
-registerHooks(useTree(src))
-registerHooks(tryAltExts)
-registerHooks(compileJsx((str, url) =>
-  ts.transpileModule(str, {
-    fileName: fileURLToPath(url),
-    compilerOptions: {
-      target: ts.ScriptTarget.ESNext,
-      module: ts.ModuleKind.ESNext,
-      jsx: ts.JsxEmit.ReactJSX,
-      sourceMap: true,
-      inlineSourceMap: true,
-      inlineSources: true,
-    }
-  }).outputText
-))
+async function run() {
+  const files = Pipeline.from(site.files)
 
-const run = async () => {
-  const mod = await import('./src/build.js')
-  mod.processSite(data, server)
+  files.with(/\.ts$/).do(f => {
+    f.path = f.path.replace(/\.ts$/, '.js')
+    f.text = stripTypeScriptTypes(f.text)
+  })
+
+  server.files = files.results()
 }
 
 run()
-src.watch().on('filesUpdated', run)
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      div: HtmlElements['div'] & { style?: string }
-    }
-  }
-}
+site.watch().on('filesUpdated', run)

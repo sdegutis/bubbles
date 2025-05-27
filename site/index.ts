3b5f9d326b0bdd29db245951a9b580a53b9ba12b
Steven Degutis <sbdegutis@gmail.com>
2025-05-26 20:30:29 -0500

    Basic demo works.

--- site/index.ts
+++ site/index.ts
@@ -1,5 +1,24 @@
 import Matter from 'matter-js'
 
-console.log('hey world', Matter)
-
 new EventSource('/reload').onmessage = () => location.reload()
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+
+const engine = Matter.Engine.create()
+
+const render = Matter.Render.create({
+  canvas,
+  engine,
+})
+
+const boxA = Matter.Bodies.rectangle(400, 200, 80, 80)
+const boxB = Matter.Bodies.rectangle(450, 50, 80, 80)
+const ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
+
+Matter.Composite.add(engine.world, [boxA, boxB, ground])
+
+Matter.Render.run(render)
+
+const runner = Matter.Runner.create()
+
+Matter.Runner.run(runner, engine)

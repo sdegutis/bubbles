3a507b83afad30dedc00b48c18553a4e6eda1a43
Steven Degutis <sbdegutis@gmail.com>
2025-05-27 17:40:24 -0500

    Variables.

--- site/ssaver.ts
+++ site/ssaver.ts
@@ -2,7 +2,11 @@ import { addCircle, canvas, clearAll, engine, Matter } from "./lib.js"
 
 engine.gravity.y = 0
 
-const sec = +(new URL(import.meta.url).searchParams.get('sec') ?? '60')
+const params = new URL(import.meta.url).searchParams
+const intsec = +(params.get('sec') ?? '60')
+const speed = +(params.get('speed') ?? '2')
+
+console.log(speed)
 
 new EventSource('/reload').onmessage = () => location.reload()
 
@@ -15,7 +19,7 @@ const restartIdleTimer = () => {
   timeout = setTimeout(() => {
     canvas.style.display = 'block'
     timeout = setInterval(addBubbles, 100)
-  }, sec * 1000)
+  }, intsec * 1000)
 }
 restartIdleTimer()
 
@@ -26,15 +30,15 @@ document.addEventListener('keydown', restartIdleTimer, { passive: true })
 document.addEventListener('wheel', restartIdleTimer, { passive: true })
 
 function addBubbles() {
-  const x = Math.random() * canvas.width
-  const y = Math.random() * canvas.height
+  const x = -10
+  const y = canvas.height + 10
 
-  const d = 10
-  const mx = Math.random() * (d * 2) - d
-  const my = Math.random() * (d * 2) - d
+  const mx = Math.random() * +speed
+  const my = Math.random() * -speed
 
-  const size = Math.random() * 10 + 20
+  const size = Math.random() * 30 + 10
 
   const circle = addCircle(x, y, size)
+  circle.frictionAir = 0
   Matter.Body.setVelocity(circle, { x: mx, y: my })
 }

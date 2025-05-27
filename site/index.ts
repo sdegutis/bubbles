3b07965275f8307af8d623ddb1173dc55485e751
Steven Degutis <sbdegutis@gmail.com>
2025-05-26 21:20:11 -0500

    No resize.

--- site/index.ts
+++ site/index.ts
@@ -3,23 +3,19 @@ import Matter from 'matter-js'
 new EventSource('/reload').onmessage = () => location.reload()
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement
+setTimeout(() => {
+  canvas.width = document.body.clientWidth
+  canvas.height = document.body.clientHeight
+})
 
 const engine = Matter.Engine.create()
 
-// engine.gravity.y = 0
+engine.gravity.y = 0
 
 const render = Matter.Render.create({
   canvas,
   engine,
 })
-
-const resize = () => {
-  canvas.width = document.body.clientWidth
-  canvas.height = document.body.clientHeight
-}
-
-resize()
-window.onresize = resize
 
 const ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true, isSensor: true })
 

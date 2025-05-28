645bea9b61246cbc6c177b352f4875a91cbc6489
Steven Degutis <sbdegutis@gmail.com>
2025-05-28 09:14:11 -0500

    Fewer.

--- site/index.ts
+++ site/index.ts
@@ -24,18 +24,16 @@ function getSizeBounds() {
 
 
 function addCircles(e: PointerEvent) {
-  for (let i = 0; i < 1 * e.pressure ** e.pressure; i++) {
-    const rand = Math.random()
+  const rand = Math.random()
 
-    const size = rand * (sizes.max - sizes.min) + sizes.min
-    const { circle, color } = addCircle(e.clientX, e.clientY, size)
-    Matter.Body.setVelocity(circle, { x: e.movementX / 10, y: e.movementY / 10 })
+  const size = rand * (sizes.max - sizes.min) + sizes.min
+  const { circle, color } = addCircle(e.clientX, e.clientY, size)
+  Matter.Body.setVelocity(circle, { x: e.movementX / 5, y: e.movementY / 5 })
 
-    const i = Math.floor(color / 360 * notes.length)
-    const note = notes[i]!
-    const octave = Math.floor((1 - rand) * 3 + 4)
-    playNote(note, octave, .3, { type: 'sine', volume: .25 })
-  }
+  const i = Math.floor(color / 360 * notes.length)
+  const note = notes[i]!
+  const octave = Math.floor((1 - rand) * 3 + 4)
+  playNote(note, octave, .1, { type: 'sine', volume: .25 })
 }
 
 canvas.onpointerdown = e => {
